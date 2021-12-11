afd3a142767a505517eb2d17b53fdd01725734fd
gdasns <95311366+gdasns@users.noreply.github.com>
2021-12-11 12:29:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 09:21:22 2021
+
+@author: Giada Sansonetto
+"""
+
 from matplotlib import pyplot as plt
+import matplotlib.dates as ydates 
 from datetime import datetime
 import json
+import math
 
 
 def mean(l):
@@ -23,17 +32,23 @@ def plotsetup(title, ylab="", xlab="", xtick=4, ytick=8, ylab2=""):
 
     fig, ax = plt.subplots()
     ax.xaxis.set_major_locator(plt.MaxNLocator(xtick))
+    ax.xaxis.set_major_locator(ydates.YearLocator())
     ax.yaxis.set_major_locator(plt.MaxNLocator(ytick))
-    plt.title(title)
-    plt.ylabel(ylab)
-    plt.xlabel(xlab)
-    plt.grid()
+    font = {'fontname':'Philosopher'}
+    plt.title(title, **font, fontsize=14)
+    plt.ylabel(ylab, **font)
+    plt.xlabel(xlab, **font)
+    #plt.xticks(**font)
+    #plt.yticks(**font)
+    plt.grid(linestyle=':')
+
 
     # sets 'ax2' only when specified
     if ylab2 != "":
         ax2 = ax.twinx()
-        ax2.set_ylabel(ylab2)
+        ax2.set_ylabel(ylab2, **font)
         ax2.xaxis.set_major_locator(plt.MaxNLocator(xtick))
+        ax2.xaxis.set_major_locator(ydates.YearLocator())
         ax2.yaxis.set_major_locator(plt.MaxNLocator(ytick))
 
         return fig, ax, ax2
@@ -90,6 +105,16 @@ def norm(l, n=1):
 
     return [e*molt for e in l]
 
+def tangent(l):
+    in_array = mc_values
+    out_array = []
+    
+    for i in range(len(mc_values)):
+        out_array.append(math.tan(in_array[i]))
+        i += 1
+    
+    return out_array
+
 
 # unpack all data in 6 lists and cuts the last two
 mc_values, mc_ts = getval(jsonr("market-cap.json")["values"])
@@ -99,7 +124,7 @@ rev_values, rev_ts = getval(jsonr("miners-revenue.json")["values"][:-5])
 
 """ part 1 task 1 """
 
-plotsetup("Market capitalization", ylab="USD")
+plotsetup("BTC Market Capitalization", ylab="USD")
 
 plt.plot(mc_ts, mc_values, color="black")
 
@@ -109,7 +134,7 @@ plt.cla()
 
 """ part 1 task 2 """
 
-plotsetup("Market capitalization (smoothed)", ylab="USD")
+plotsetup("Market Capitalization (smoothed)", ylab="USD")
 
 plt.plot(mc_ts, smooth(mc_values), color="black")
 plt.plot(mc_ts, mc_values, color="black", alpha=0.2)
@@ -120,7 +145,7 @@ plt.cla()
 
 """ part 2 task 1 """
 
-plotsetup("Miner revenues", ylab="USD")
+plotsetup("Miners' Revenues", ylab="USD")
 
 plt.plot(rev_ts, smooth(rev_values, n=7), color="black")
 
@@ -143,7 +168,7 @@ plt.cla()
 
 """ part 2 task 3 """
 
-plotsetup("Reward / difficulty")
+plotsetup("Revenues / Difficulty")
 
 # list with the ratios normalized
 ratio_values = norm([rev_values[i] / diff_values[i] for i in range(len(diff_values))])
@@ -155,7 +180,7 @@ plt.cla()
 
 """ part 2 task 4 """
 
-plotsetup("10% best return periods")
+plotsetup("10% Best Return Periods")
 
 # gets how many are 10% of all days
 elem_required = int(0.1*len(ratio_values))
@@ -167,6 +192,34 @@ for i in range(len(ratio_values)):
     if ratio_values[i] in highest_values:
         plt.axvline(x=diff_ts[i], color="orange", alpha=1)
 
-plt.plot(diff_ts, smooth(ratio_values, n=7), color="black")
+plt.plot(diff_ts, smooth(ratio_values, n=7), color="black", linewidth=1.5, alpha=1)
 
 plt.savefig("part2_task4.png", dpi=600)
+
+'''Additional insight
+    by normalizing the BTC market cap,
+    we compare periods of best returns
+    with the market capitalization.
+    How is the market performing when 
+    there are the highest profits.'''
+    
+slope = smooth(tangent(smooth(mc_values)))
+
+plotsetup('Higher revenues')
+
+# the highest 10%
+
+twopercent = int(0.1*len(slope))
+higherslope = sorted(slope)[-twopercent:]
+
+for i in range(len(mc_ts)):
+    timestamps = []
+    if slope[i] in higherslope:
+        timestamps.append(slope[i])
+        plt.axvline(x = mc_ts[i], color='orange', linewidth = 1.5) 
+
+plt.plot(mc_ts, smooth(mc_values), color="black")
+
+plt.savefig('high rev.png', dpi = 600)
+
+print(datetime.fromtimestamp(timestamps))
